960decfb1cbfdd078c44e6e922100b345cde9d91
ATZCDPGAL <atzincp@gmail.com>
2022-10-28 13:30:46 -0500

    Agregar archivos de proyecto.

---
+++ MysqlDataBase/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace MysqlDataBase
+{
+    public partial class Form1 : Form
+    {
+        //Agregamos la base de datos
+        //La base de datos se crea en la carpeta DEBUG
+        string path = "data_table.db";
+        string cs = @"URI=file:" + Application.StartupPath + "\\data_table.db";
+        
+        SQLiteConnection conn;
+        SQLiteCommand cmd;
+        SQLiteDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Create_db();
+            data_show();
+        }
+
+        private void Create_db()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source="+path))
+                {
+                    sqlite.Open();
+                    string sql = "create table test(name varchar(20), id varchar(12))";
+                    SQLiteCommand command = new SQLiteCommand(sql,sqlite);
+                    command.ExecuteNonQuery();
+                } 
+            }
+            else
+            {
+                Console.WriteLine("La base de datos no se puede actualizar");
+                return;
+            }
+        }
+
+        private void data_show()
+        {
+            var conn = new SQLiteConnection(cs);
+            conn.Open();
+
+            string stm = "SELECT * FROM test";
+            var cmd = new SQLiteCommand(stm, conn);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Insert(0, dr.GetString(0), dr.GetString(1));
+            }    
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
